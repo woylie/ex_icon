2e1ec32dfbfcc463b8e78a3942f79ce82b825df5
woylie <13847569+woylie@users.noreply.github.com>
2026-04-10 20:34:33 +0200

    add Simple Icons provider

--- .ex_icon.exs
+++ .ex_icon.exs
@@ -6,5 +6,13 @@
     module_path: "output/lucide.ex",
     module_name: MyAppWeb.Components.Lucide,
     attrs: ["stroke"]
+  ],
+  simple_icons: [
+    icons: ["helix", "mastodon"],
+    provider: ExIcon.SimpleIcons,
+    version: "16.15.0",
+    module_path: "output/simple_icons.ex",
+    module_name: MyAppWeb.Components.SimpleIcons,
+    attrs: []
   ]
 ]
